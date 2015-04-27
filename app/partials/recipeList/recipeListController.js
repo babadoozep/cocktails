367c18b1324c05e0687192d353adc83a3d88f11a
babadoozep <pdxmichel@gmail.com>
2015-04-26 20:11:00 -0700

    Add filtering using a search field as well as drop down menu for base spirit (i.e. Vodka).

--- app/partials/recipeList/recipeListController.js
+++ app/partials/recipeList/recipeListController.js
@@ -2,7 +2,16 @@ recipeControllers.controller('ListController', ['$scope', '$http', function($sco
     $http.get('app/data.json').success(function(data) {
         $scope.recipe = data;
         $scope.recipeOrder = 'name';
+        $scope.sortBySpirit = 'spirit';
         $scope.imgUrl = "assets/img/list.jpg";
+
+
+        $scope.spirits = [];
+        angular.forEach($scope.recipe, function(value, index){
+            if (this.indexOf(value.spirit)===-1) {
+                this.push(value.spirit);
+            }
+        }, $scope.spirits);
     });
 }]);
 
